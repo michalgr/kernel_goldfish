d5d71b1669fdf5d0768f9e8e08a37cd23d0f93db
Alexei Starovoitov <ast@fb.com>
2016-07-06 22:38:36 -0700

    bpf: introduce bpf_get_current_task() helper
    over time there were multiple requests to access different data
    structures and fields of task_struct current, so finally add
    the helper to access 'current' as-is. Tracing bpf programs will do
    the rest of walking the pointers via bpf_probe_read().
    Note that current can be null and bpf program has to deal it with,
    but even dumb passing null into bpf_probe_read() is still safe.
    
    Change-Id: I11fc29a2154c5dde89ac732905fcc7fbb5f9edd4
    Suggested-by: Brendan Gregg <brendan.d.gregg@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -268,6 +268,17 @@ static const struct bpf_func_proto bpf_perf_event_output_proto = {
 	.arg5_type	= ARG_CONST_STACK_SIZE,
 };
 
+static u64 bpf_get_current_task(u64 r1, u64 r2, u64 r3, u64 r4, u64 r5)
+{
+	return (long) current;
+}
+
+static const struct bpf_func_proto bpf_get_current_task_proto = {
+	.func		= bpf_get_current_task,
+	.gpl_only	= true,
+	.ret_type	= RET_INTEGER,
+};
+
 static const struct bpf_func_proto *tracing_func_proto(enum bpf_func_id func_id)
 {
 	switch (func_id) {
@@ -285,6 +296,8 @@ static const struct bpf_func_proto *tracing_func_proto(enum bpf_func_id func_id)
 		return &bpf_tail_call_proto;
 	case BPF_FUNC_get_current_pid_tgid:
 		return &bpf_get_current_pid_tgid_proto;
+	case BPF_FUNC_get_current_task:
+		return &bpf_get_current_task_proto;
 	case BPF_FUNC_get_current_uid_gid:
 		return &bpf_get_current_uid_gid_proto;
 	case BPF_FUNC_get_current_comm:
